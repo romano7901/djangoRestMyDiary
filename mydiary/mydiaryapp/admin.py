cdaf17958bcdcf396940827fc8b4b0f94693ddf0
romano <romano@ifiles.ru>
2022-01-10 11:08:06 +0300

    minus patient filter

--- mydiary/mydiaryapp/admin.py
+++ mydiary/mydiaryapp/admin.py
@@ -9,17 +9,19 @@ class PatientAdmin(admin.ModelAdmin):
     list_display_links = ('fullName', 'policy')
     search_fields = ('fullName', 'policy')
 
+
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('resourceId','doctorName', 'specialityName', 'cabNum', 'duration')
+    list_display = ('resourceId', 'doctorName', 'specialityName', 'cabNum', 'duration')
     list_filter = ('doctorName', 'specialityName')
     list_display_links = ('doctorName', 'specialityName')
     search_fields = ('doctorName', 'specialityName')
 
+
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('resourceId', 'patientId', 'recordDate')
-    list_filter = ('resourceId', 'patientId', 'recordDate')
-    list_display_links =  ('resourceId', 'patientId','recordDate')
+    list_display_links = ('resourceId', 'patientId', 'recordDate')
     search_fields = ('resourceId', 'patientId')
+
 
 admin.site.register(Patient, PatientAdmin)
 admin.site.register(Resource, ResourceAdmin)
