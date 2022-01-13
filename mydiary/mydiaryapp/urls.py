d0ec24f7a8c1fe03e5d7c7429a24c894aa7aa266
romano <romano@ifiles.ru>
2022-01-13 11:02:25 +0300

    fix and rename method add to save

--- mydiary/mydiaryapp/urls.py
+++ mydiary/mydiaryapp/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('patients/<str:id>/', PatientListById.as_view()),
     path('patients/', PatientList.as_view()),
     path('resources/', ResourceList.as_view()),
-    path('add/', RecordAdd.as_view()),
-    path('delete/', RecordDel.as_view()),
+    path('save', RecordAdd.as_view()),
+    path('delete', RecordDel.as_view()),
     path('dates/<str:resources>/',DatesAPIView.as_view()),
   ]
 
